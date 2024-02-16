97474dfca2bb18c3edc3b0962bcbc25d36347acc
Swapnil123123 <swapnildpawar46@gmail.com>
2024-02-16 14:25:13 +0530

    updated the code

--- script.js
+++ script.js
@@ -2,16 +2,21 @@ async function fetchWeatherData(cityName) {
 	let url=`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=1c84b526829ff1e1ce9585567ae57e91`
 	const responce = await fetch (url)
 	const data = await responce.json();
-	console.log(data.name)
-	
+	let city = document.getElementById("city");
+     city.innerHTML= data.name;
+	let windSpeed =document.getElementById("wind_speed");
+	windSpeed.innerHTML = data.wind.speed + " km/hr";
+	let humidity = document.getElementById("humidity");
+	humidity.innerHTML = data.main.humidity+" g/kg" ;
+	let temp = document.getElementById("temp");
+	temp.innerHTML = Math.round((data.main.temp)-273.15)+"°C";
+	console.log(data);	
 }
-
 function fetchCity(){
 	let cityName = document.getElementById("city_name");
-	if (cityName.value==="") {
-		alert("Enter a city name")
-	}
-	else{
+	if (cityName.value===""){
+		alert("Enter a city name");
+	}else{
 		fetchWeatherData(cityName.value);
 		cityName.value="";
 	}
